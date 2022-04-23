254fedffd0d649ab127f09743a7706e840ed2139
Dan <dany.condurari@gmail.com>
2022-04-24 00:41:16 +0300

    search

--- src/pages/weather.js
+++ src/pages/weather.js
@@ -1,27 +1,33 @@
 import {useState, useEffect} from "react";
 import useHoroscop from "../hooks/useHoroscop";
+import { useSearchParams } from "react-router-dom";
 
 const Weather = () => {
 
-    const [searchWeather, setSearchWeather] = useState("")
+    const [searchWeather, setSearchWeather] = useState("");
 
+    const [url, setUrl] = useState("");
+
+
+    useEffect(() =>{        
+
+    }, [])
+   
     
     
-   
-   
-    //const {data, error, isPending} = useHoroscop(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=8e352e5fc385213cf324146681cff4bb`)
+    
+    
+    //https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=8e352e5fc385213cf324146681cff4bb
+    
+    
     
     const submit = (e) =>{
         e.preventDefault();
         
-        const queryParams = new URLSearchParams(searchWeather);
-        const query = queryParams.get("q");
-
-        console.log(searchWeather)
-        
-    
+        setUrl(`https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&appid=8e352e5fc385213cf324146681cff4bb`)
     }
-
+    
+    console.log(data)
 
     return ( 
         <div>
@@ -29,6 +35,10 @@ const Weather = () => {
              <input onChange={(e) => setSearchWeather(e.target.value)} type="text" />
               <button>Search</button>
           </form>
+
+
+          {data && <p>{data}</p>}
+          {error && <p>{error}</p>}
             
 
         </div>
